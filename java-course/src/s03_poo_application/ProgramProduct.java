b186641cd712a4c682999277a86e1a1b1bae0e25
David Cordeiro Moura <david.mourabr@gmail.com>
2026-02-23 14:02:13 -0300

    aulas novas de poo

---
+++ java-course/src/s03_poo_application/ProgramProduct.java
@@ -0,0 +1,29 @@
+package s03_poo_application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import s03_poo_entities.Product;
+
+public class ProgramProduct {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter Product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		
+		sc.close();
+	}
+
+}
