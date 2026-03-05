05ca77845bc7b4c5f4a031e10670368bf14ed0a2
David Cordeiro Moura <david.mourabr@gmail.com>
2026-03-04 21:44:33 -0300

    up

---
+++ java-course/src/s04_poo_membrosEstaticos_exercicios_application/Program.java
@@ -0,0 +1,33 @@
+package s04_poo_membrosEstaticos_exercicios_application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("What is the dollar price? ");
+		double PriceDollar = sc.nextDouble();
+		
+		System.out.print("How many dollars will bought? ");
+		double dollar = sc.nextDouble();
+		
+		System.out.printf("Amount to be paid in reais = %.2f");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
--- java-course/src/s04_poo_membrosEstaticos_exercicios_util/CurrencyConverter.java
+++ java-course/src/s04_poo_membrosEstaticos_exercicios_util/CurrencyConverter.java
@@ -1,5 +1,10 @@
 package s04_poo_membrosEstaticos_exercicios_util;
 
 public class CurrencyConverter {
-
+	
+	public static double Converter(double ValorReais) {
+		return dollar * PriceDollar;
+	}
+	
+	
 }
